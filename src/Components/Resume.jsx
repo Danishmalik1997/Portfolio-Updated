238fe820a3c56d17e58c1e07d8c3e1dc9c132c18
Danish Malik: git config --global user.name Danish <danishahmed888@gmail.com>
2023-09-14 16:14:13 +0530

    Finally

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import MyResume from '../resources/Danish_Resume.pdf'
+import NewResume from '../resources/Danish_Resume.pdf'
 import { Button } from "@mui/material";
 import DownloadIcon from '@mui/icons-material/Download';
 
 const Resume = () => {
   return (
     <div className="p-2">
-      <Button variant="contained" color="inherit"> <a href={MyResume} download={MyResume} > <DownloadIcon/> Download Resume </a> </Button>
+      <Button variant="contained" color="inherit"> <a href={NewResume} download={NewResume} > <DownloadIcon/> Download Resume </a> </Button>
     </div>
   );
 };
